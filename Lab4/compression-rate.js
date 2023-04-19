5816f90ac354a31bbba0395a39b446db2a67a367
KTomy <kisstomi7.kt@gmail.com>
2023-04-19 14:47:39 +0300

    Delete Lab4

--- Lab4/compression-rate.js
+++
@@ -1,5 +0,0 @@
-module.exports = (clear, encodingTable, encoded) => {
-  const originalSize = clear.length * 8; // 8 bits per character
-  const encodedSize = encoded.length;
-  return 100 - encodedSize / originalSize * 100;
-};
